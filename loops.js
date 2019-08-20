0bb33400fca479c097fb4c40109a57151bf9b5ea
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-20 23:38:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,13 @@ function whileLoop(number) {
   return "done"
 }
 
+function doWhileLoop(num) {
+  function incrementVariable() {
+    var i = 0
+    i += 1
+    return i
+  }
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num)
+}
